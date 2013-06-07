76c5e767febe8ce32f87d082eaba8517f4ed0324
viksit arora <enliven.viksit@gmail.com>
2013-06-06 20:14:55 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artisania::Application.config.secret_token = '4609393f1c012dd01d168ada05fc5751011e86293fc61dd8874f96777f6d25d47d8f9e567f874ec25c0d6bff480cbfdb057194a25b4d44f8ef42e717b5301f19'
