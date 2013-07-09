e33dc04a112ff049839934a62360fd850be1293c
viksit arora <enliven.viksit@gmail.com>
2013-07-09 04:46:07 -0700

    nested partial working

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,6 +1,6 @@
 class Attribute < ActiveRecord::Base
 
-  attr_accessible :name, :attribute_layer
+  attr_accessible :name
   
   has_attached_file :attr_img, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
--- app/models/attribute_layer.rb
+++ app/models/attribute_layer.rb
@@ -1,9 +1,10 @@
 class AttributeLayer < ActiveRecord::Base
 
-  attr_accessible :name, :palette, :attributes_attributes
+
+  attr_accessible :name
 
   belongs_to :palette
   has_many   :attributes
-  accepts_nested_attributes_for :attributes
+  accepts_nested_attributes_for :attributes, :reject_if => :all_blank, :allow_destroy => true
   
 end
