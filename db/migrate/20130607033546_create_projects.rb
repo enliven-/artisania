da47ffc5dd0d3b11ccf72b2f116d9650f073f345
viksit arora <enliven.viksit@gmail.com>
2013-07-12 02:19:00 -0700

    cleaned up migrations

--- db/migrate/20130607033546_create_projects.rb
+++ db/migrate/20130607033546_create_projects.rb
@@ -3,6 +3,9 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string       :name
       t.text         :description
+      t.integer      :user_id
+      t.integer      :design_versions_count, :default => 0
+
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130712022150) do
-
+ActiveRecord::Schema.define(:version => 20130711004800) do
 
   create_table "attribute_layers", :force => true do |t|
     t.string   "name"
@@ -71,13 +69,14 @@ ActiveRecord::Schema.define(:version => 20130712022150) do
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.integer  "user_id"
+    t.integer  "design_versions_count", :default => 0
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
     t.string   "img_file_file_name"
     t.string   "img_file_content_type"
     t.integer  "img_file_file_size"
     t.datetime "img_file_updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
